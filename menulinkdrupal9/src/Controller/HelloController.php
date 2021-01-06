2745b6c2b1e26e96d94404dc21d3a6ae4d2551ca
Andrea Marrin <andrea@cangurin.com>
2021-01-06 21:09:18 +0100

    mi primer commit

---
+++ menulinkdrupal9/src/Controller/HelloController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\hello_world\Controller\HelloController.
+ */
+namespace Drupal\hello_world\Controller;
+class HelloController {
+  public function content() {
+    return array(
+      '#type' => 'markup',
+      '#markup' => t('Hello, World!'),
+    );
+  }
+
+}
